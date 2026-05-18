9dc1625c4a3ff3234c32a60b04766846dd65b266
Neme710 <150207986+Neme710@users.noreply.github.com>
2026-05-18 09:16:12 -0400

    Add user management functionality in main.py

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-#Activity 3.2.2 Step 7
 from Post import Post
 
 all_posts_archive = []
+usernames = ["aiden", "aidan", "aden", "adan"]
+new_post():
+  pass
+
+remove_post():
+  pass
+
+
 
 username = input("Please input youyr user name here - ")
+if username in usernames:
+  print ("hello", username)
+  user_choice = int(input("Would you like to make new post (1) or remove a previos one (2)"))
+  match user_choice:
+    case 1:
+      print (1)
+      new_post()
+    case 2:
+      print(2)
+      remove_post()
+    case _:
+      print("Wrong input")
+      
+      
+
+
