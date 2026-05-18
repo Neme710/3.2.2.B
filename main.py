5bcfc8254b1db905956ffa05aed855dfbb0e9dfd
Neme710 <150207986+Neme710@users.noreply.github.com>
2026-05-18 11:16:46 -0400

    Correct post archive handling and user input
    Fix syntax errors and improve post handling logic.

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from Post import Post
+
 all_posts_archive = []
 usernames = ["aiden", "aidan", "aden", "adan"]
+
 def new_post():
   new_post_input = input("New Post's txt here --")
-  all_post_archive.append(Post(username, new_post_input)
+  all_post_archive.append(Post(username, new_post_input))
   #Dont know the proper sytenx for the post method
+                          
 def remove_post():
   for index, value in enumerate(all_posts_archive):
     print(f"Index: {index}, Value: {value}")
   remove_post_input = int(input("Please selcect the post you would like to remove --"))
   del all_posts_archive [remove_post_input]
-username = input("Please input youyr user name here - ")
-for username in usernames:
+username = input("Please input your user name here - ")
+
+while username in usernames:
   print ("hello", username)
   user_choice = int(input("Would you like to make new post (1) or remove a previos one (2)"))
   match user_choice:
@@ -23,4 +27,4 @@ for username in usernames:
       remove_post()
     case _:
       print("Wrong input")
-    print (all_post_archive)
+  print (all_post_archive)
