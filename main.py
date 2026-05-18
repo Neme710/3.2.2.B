a9b3e14728b5c46c877990ca9e42cfa138263a23
Neme710 <150207986+Neme710@users.noreply.github.com>
2026-05-18 10:13:01 -0400

    Correct post creation and removal logic
    Fix syntax errors and update post handling logic.

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 from Post import Post
-
 all_posts_archive = []
 usernames = ["aiden", "aidan", "aden", "adan"]
 def new_post():
   new_post_input = input("New Post's txt here --")
-  Post(username, new_post_input)
-
-  
-
+  all_post_archive.append(Post(username, new_post_input)
+  #Dont know the proper sytenx for the post method
 def remove_post():
-  pass
-
-
-
+  for index, value in enumerate(all_posts_archive):
+    print(f"Index: {index}, Value: {value}")
+  remove_post_input = int(input("Please selcect the post you would like to remove --"))
+  del all_posts_archive [remove_post_input]
 username = input("Please input youyr user name here - ")
-if username in usernames:
+for username in usernames:
   print ("hello", username)
   user_choice = int(input("Would you like to make new post (1) or remove a previos one (2)"))
   match user_choice:
@@ -26,7 +23,4 @@ if username in usernames:
       remove_post()
     case _:
       print("Wrong input")
-      
-      
-
-
+    print (all_post_archive)
